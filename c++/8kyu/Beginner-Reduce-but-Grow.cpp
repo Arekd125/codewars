24b4f72f8f65c20d65e7d76721403ec287fdc5e8
Arekd125 <arekdom125@gmail.com>
2023-02-10 00:01:39 +0100

    add new solutions

---
+++ c++/8kyu/Beginner-Reduce-but-Grow.cpp
@@ -0,0 +1,12 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+#include <vector>
+int grow(std::vector<int> nums)
+{
+    int result = 1;
+    for (int num : nums)
+        result *= num;
+    return result;
+}
