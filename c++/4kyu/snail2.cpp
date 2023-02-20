d1d8047ea680e9e2747049f61aae54b773a16525
Arekd125 <arekdom125@gmail.com>
2023-02-20 01:06:31 +0100

    add working solution snail

--- c++/4kyu/snail2.cpp
+++ c++/4kyu/snail2.cpp
@@ -25,10 +25,19 @@ int main()
     std::vector<std::vector<int>> b = {{1}};
     std::vector<std::vector<int>> c = {{1, 2}, {4, 3}};
     std::vector<std::vector<int>> d = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
-    std::vector<std::vector<int>> e = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
-    std::vector<std::vector<int>> f = {{1, 2, 3, 4, 5}, {16, 17, 18, 19, 6}, {15, 24, 25, 20, 7}, {14, 23, 22, 21, 8}, {13, 12, 11, 10, 9}};
+    std::vector<std::vector<int>> e = 
+    {{1, 2, 3, 4},
+    {12, 13, 14, 5},
+    {11, 16, 15, 6},
+    {10, 9, 8, 7}};
+    std::vector<std::vector<int>> f = 
+    {{1, 2, 3, 4, 5}, 
+    {16, 17, 18, 19, 6},
+    {15, 24, 25, 20, 7},
+    {14, 23, 22, 21, 8},
+    {13, 12, 11, 10, 9}};
 
-    result = snail(f);
+    result = snail(e);
     show_vector(result);
 }
 
@@ -37,35 +46,61 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map)
     std::vector<int> result;
     size_t n{snail_map.size()};
     size_t k{n - 1};
-    size_t z = 0;
-    // const int *wsk_s_m{&snail_map[0][0]};
+    int  row= 0;
+    int  col = 0;
+    int  row2 =4;
 
-    for (int i = 0; i <= 4; i++)
-    {
-        result.push_back(snail_map[z][i]);
-    }
-    z++;
-    for (int i = 1; i <k; i++)
-    {
-        result.push_back(snail_map[i][k]);
-    }
     
-    for (int i = k; i >1; i--)
+
+    for (int i = 0; i < n; i++)
     {
-        result.push_back(snail_map[k][i]);
+        result.push_back(snail_map[row][col++]);
     }
+    col--;
+     
+
+    do
+    {
+        for (int i = 0; i <k; i++)
+    {
+        result.push_back(snail_map[++row][col]);
+    }
+   
+    for (int i = 0; i <k; i++)
+    {
+        result.push_back(snail_map[row][--col]);
+    }
+    k--;
+    for (int i = 0; i <k; i++)
+    {
+        result.push_back(snail_map[--row][col]);
+    }
+    for (int i = 0; i < k; i++)
+    {
+        result.push_back(snail_map[row][++col]);
+        
+    }
+    k--;
+    } while (k!=0);
+    
+
+    
+    // for (int i = k; i >1; i--)
+    // {
+    //     result.push_back(snail_map[k][i]);
+    // }
          
-     k--    ;
-    for (int i = k; i >=0; i--)
-    {
-        result.push_back(snail_map[k][i]);
-    }
+    //  k--    ;
+    // for (int i = k; i >=0; i--)
+    // {
+    //     result.push_back(snail_map[k][i]);
+    // }
     
-    z++;
-    for (int i = k; i >= 1; i--) // LEFT
-    {
-        result.push_back(snail_map[i][0]);
-    }
+    // z++;
+    // for (int i = k; i >= 1; i--) // LEFT
+    // {
+    //     result.push_back(snail_map[i][0]);
+    // }
 
     return result;
 }
