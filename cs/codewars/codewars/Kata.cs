d8a785f912783adf926b4133171f283db91a3fc4
Arekd125 <arekdom125@gmail.com>
2024-01-15 16:20:49 +0100

    Find the divisors

--- cs/codewars/codewars/Kata.cs
+++ cs/codewars/codewars/Kata.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,28 @@ namespace codewars
 {
     internal class Kata
     {
+        public static int[] Divisors(int n)
+        {
+            List<int> result = new List<int>();
+
+            int square = (int)Math.Sqrt(n);
+
+            for (int i = 2; i <= square; i++)
+            {
+                if (n % i == 0)
+                {
+                    result.Add(i);
+                    if (i != n / i)
+                    {
+                        result.Add(n / i);
+                    }
+                }
+            }
+            if (result.Count == 0)
+                return null;
+
+            result.Sort();
+            return result.ToArray();
+        }
     }
 }
--- cs/codewars/codewars/UnitTest1.cs
+++ cs/codewars/codewars/UnitTest1.cs
@@ -3,5 +3,27 @@ namespace codewars
     public class Tests
     {
         [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] { 3, 5 }, Kata.Divisors(15));
+        }
+
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(new int[] { 2, 4, 8 }, Kata.Divisors(16));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(new int[] { 11, 23 }, Kata.Divisors(253));
+        }
+
+        [Test]
+        public void SampleTest3()
+        {
+            Assert.AreEqual(new int[] { 2, 3, 4, 6, 8, 12 }, Kata.Divisors(24));
+        }
     }
 }
